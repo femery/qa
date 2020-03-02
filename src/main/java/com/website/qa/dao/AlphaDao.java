8e60a93faf204019f3df5d763bd8cf5eec6e01b5
he <ferrury@126.com>
2020-03-02 11:57:35 +0800

    test

---
+++ src/main/java/com/website/qa/dao/AlphaDao.java
@@ -0,0 +1,7 @@
+package com.website.qa.dao;
+
+public interface AlphaDao {
+
+    String select();
+
+}
---
+++ src/test/java/com/website/qa/QaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.website.qa;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
